45bdab8706d6b522d418e959798f6141dfe0f8c7
Luis David <ld0110mp@gmail.com>
2025-08-11 19:49:48 -0600

    feat: Agrega clase record para deserialización de datos de Temporadas

--- 02-java-y-spring-boot/01-lambdas-streams-spring/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ 02-java-y-spring-boot/01-lambdas-streams-spring/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package com.aluracursos.screenmatch;
 
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -18,7 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApí = new ConsumoAPI();
-			var json = consumoApí.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=367d7215");
+		var json = consumoApí.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=367d7215");
 		System.out.println(json);
 
 		ConvierteDatos conversor = new ConvierteDatos();
@@ -28,5 +32,16 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		json = consumoApí.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=367d7215");
 		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
 		System.out.println(episodios);
+
+		List<DatosTemporadas> temporadas = new ArrayList<>();
+		for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
+			json = consumoApí.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=" + i + "&apikey=367d7215");
+			var datosTemporada = conversor.obtenerDatos(json, DatosTemporadas.class);
+			temporadas.add(datosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
+
+
 	}
 }
